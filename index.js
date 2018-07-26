e1bb92f7f2413986d6c9d6cfe8276bc18e151d6d
6Jonathan6 <jpr1993mm@hotmail.com>
2018-07-26 14:12:04 -0500

    testing code Build

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     // TODO implement
     console.log("Esto está funcionndo")
-    console.log("Esto está funcionndo")
+    console.log("Esto esta funcionndo")
     return 'El pejes rules!';
 };
